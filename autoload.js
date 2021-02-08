7aeae46b6a9f6bc0f4018795bb4924c74cf0c6d8
chenyu521 <54203080+chenyu521@users.noreply.github.com>
2021-02-08 17:27:31 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://live2d-api.chenyu520.cn"
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			cdnPath: "https://live2d-api.chenyu520.cn"
 		});
 	});
 }
